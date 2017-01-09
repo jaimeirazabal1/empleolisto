01942b1ba50756de70566e0bbf5b78dc2f6d0823
Jaime Irazabal <jaimeirazabal1@gmail.com>
2017-01-09 16:08:41 -0430

    haciendo pantalla de super admin para agregar empresas, falta el responsive

--- default/app/controllers/RS_controller.php
+++ default/app/controllers/RS_controller.php
@@ -5,7 +5,16 @@ class RSController extends AppController{
 
 	}
 	public function login(){
-		
+		//print_r($_POST);
+		if (Input::hasPost("user")) {
+			if (Input::post("user") == "superadmin" and Input::post("password") == "superadmin") {
+				Router::toAction("superadmin");
+			}else{
+				Flash::error("Nombre de usuario o contraseña inválidos!");
+			}
+		}
 	}
+	public function superadmin(){
 
+	}
 }
